feee9290205c8ffc05ba556b3338b633ca0fd521
Sopazi101 <LSopazi@retrorabbit.co.za>
2025-10-21 15:20:54 +0200

    init

---
+++ Code Katas/Fizz_Buzz.py
@@ -0,0 +1,26 @@
+def fizzBuzz(number):
+    
+    if isinstance(number, float):
+        
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+            return
+
+        if number % 3 != 0 and number % 5 != 0:
+            print(number)
+            return
+
+        if number % 3 == 0:
+            print("Fizz")
+            return
+
+        if number % 5 == 0:
+            print("Buzz")
+            return
+    else:
+        print("Input is not an integer.")
+        return
+
+
+if __name__ == "__main__":
+    fizzBuzz(3)
